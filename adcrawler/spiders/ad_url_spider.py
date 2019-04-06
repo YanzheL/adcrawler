f3999bf935b43df5489c665bc56cb51bc34f0300
YanzheL <lee.yanzhe@yanzhe.org>
2019-04-05 19:59:38 -0700

    read settings from spider

--- adcrawler/spiders/ad_url_spider.py
+++ adcrawler/spiders/ad_url_spider.py
@@ -18,9 +18,9 @@ class AdUrlSpider(AdSpiderBase):
             'adcrawler.pipelines.url_task.UrlPipeline': 300,
         }
     }
-    MAX_URL_TASKS = 20000
-    RECURSIVE_CHECK_DEPTH = 4
-    MAX_TASK_DEPTH = 100
+    DEFAULT_MAX_URL_TASKS = 20000
+    DEFAULT_RECURSIVE_CHECK_DEPTH = 4
+    DEFAULT_MAX_TASK_DEPTH = 100
 
     def make_request_from_task(self, serialized_task, **kwargs):
         serialized_task = str(serialized_task, 'utf8')
@@ -29,7 +29,7 @@ class AdUrlSpider(AdSpiderBase):
             url=task['url'],
             **kwargs
         )
-        if task['cur_depth'] > self.MAX_TASK_DEPTH:
+        if task['cur_depth'] > self.settings.get('MAX_TASK_DEPTH', self.DEFAULT_MAX_TASK_DEPTH):
             return
         request.meta['cur_depth'] = task['cur_depth']
         request.meta['splash'] = {
@@ -37,7 +37,6 @@ class AdUrlSpider(AdSpiderBase):
                 # set rendering arguments here
                 'html': 1,
                 'wait': 10
-                # 'png': 1,
 
                 # 'url' is prefilled from request url
                 # 'http_method' is set to 'POST' for POST requests
@@ -72,7 +71,7 @@ class AdUrlSpider(AdSpiderBase):
             item['ad_img_urls'] = [ad_src]
             yield item
         all_a_tags = soup.find_all('a')
-        if self.server.llen(self.redis_key) > self.MAX_URL_TASKS:
+        if self.server.llen(self.redis_key) > self.settings.get('MAX_URL_TASKS', self.DEFAULT_MAX_URL_TASKS):
             return
         for at in all_a_tags:
             a_url = at.attrs.get('href', None)
@@ -84,31 +83,17 @@ class AdUrlSpider(AdSpiderBase):
             next_task['cur_depth'] = response.meta['cur_depth'] + 1
             yield next_task
 
-    @staticmethod
-    def maybe_ad_img(img_tag, cur_depth=0):
-        if cur_depth >= AdUrlSpider.RECURSIVE_CHECK_DEPTH:
+    def maybe_ad_img(self, img_tag, cur_depth=0):
+        if cur_depth >= self.settings.get('RECURSIVE_CHECK_DEPTH', self.DEFAULT_RECURSIVE_CHECK_DEPTH):
             return False
-        if AdUrlSpider.has_ad(img_tag):
+        if self.has_ad(img_tag):
             return True
-        if AdUrlSpider.sibling_has_ad(img_tag):
+        if self.sibling_has_ad(img_tag):
             return True
-        return AdUrlSpider.maybe_ad_img(img_tag.parent, cur_depth + 1)
+        return self.maybe_ad_img(img_tag.parent, cur_depth + 1)
 
-    @staticmethod
-    def get_best_a_tag(a_tags):
-        tag = None
-        href = None
-        for a_tag in a_tags:
-            if 'href' not in a_tag.attrs:
-                continue
-            if tag is None or len(a_tag.attrs['href']) > len(tag.attrs['href']):
-                tag = a_tag
-                href = a_tag.attrs['href']
-        return tag, href
-
-    @staticmethod
-    def get_img_ad_url(img_tag, cur_depth=0):
-        if cur_depth >= AdUrlSpider.RECURSIVE_CHECK_DEPTH:
+    def get_img_ad_url(self, img_tag, cur_depth=0):
+        if cur_depth >= self.settings.get('RECURSIVE_CHECK_DEPTH', self.DEFAULT_RECURSIVE_CHECK_DEPTH):
             return
         if not isinstance(img_tag, Tag):
             return
@@ -121,10 +106,22 @@ class AdUrlSpider(AdSpiderBase):
         if parent.name == 'a' and 'href' in parent.attrs:
             return parent.attrs['href']
         sibling_a_tags = parent.find_all("a", recursive=False)
-        best_a, best_href = AdUrlSpider.get_best_a_tag(sibling_a_tags)
+        best_a, best_href = self.get_best_a_tag(sibling_a_tags)
         if not best_href:
-            return AdUrlSpider.get_img_ad_url(img_tag.parent, cur_depth + 1)
+            return self.get_img_ad_url(img_tag.parent, cur_depth + 1)
         return best_href
+
+    @staticmethod
+    def get_best_a_tag(a_tags):
+        tag = None
+        href = None
+        for a_tag in a_tags:
+            if 'href' not in a_tag.attrs:
+                continue
+            if tag is None or len(a_tag.attrs['href']) > len(tag.attrs['href']):
+                tag = a_tag
+                href = a_tag.attrs['href']
+        return tag, href
 
     @staticmethod
     def has_ad(element):
